7ad77c205d37e89afcfe1e7d6938841b984d80b6
Abhishek Hulage <abhihulage14@gmail.com>
2024-06-25 10:48:15 +0530

    learned about array

--- src/DataTypes/dataTypes.ts
+++ src/DataTypes/dataTypes.ts
@@ -1,7 +1,10 @@
-let sales = 123_456_789;
-let course = 'TypeScript';
-let is_published = true;
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published = true;   // if you are providing value to varibale then don't need to mention type, ts will know what type of variable it is
 
 let level;  // if we don't initialize the variable then typescript consider it as "any" type and avoid using any type
 
+
 // Arrays 
+let numbers: number[] = []; // if array is not initialized then you must define the type of the array
+numbers[0] = 1;
