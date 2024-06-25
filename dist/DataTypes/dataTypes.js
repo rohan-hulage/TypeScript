8d3143dccee3a97fbaffd0844f0a41a419789a8f
Abhishek Hulage <abhihulage14@gmail.com>
2024-06-25 11:36:51 +0530

    Learned about functions in ts

--- dist/DataTypes/dataTypes.js
+++ dist/DataTypes/dataTypes.js
@@ -5,8 +5,26 @@ let is_published = true;
 let level;
 let numbers = [];
 numbers[0] = 1;
+let names = ["Abhishek", "Adi"];
+names.push("Pratik");
+console.log("Arrays:");
+console.log(names);
 let details = [1, "Abhishek"];
+console.log("Touples:");
+console.log(details);
 ;
 let mySize = 3;
+console.log("Enum:");
 console.log(mySize);
+function incomeTax(income) {
+    if (income <= 50000) {
+        return income * 0.1;
+    }
+    else {
+        return income * 0.3;
+    }
+}
+console.log("Functions");
+const abhiTax = incomeTax(100000);
+console.log(abhiTax);
 //# sourceMappingURL=dataTypes.js.map
--- src/DataTypes/dataTypes.ts
+++ src/DataTypes/dataTypes.ts
@@ -8,11 +8,34 @@ let level;  // if we don't initialize the variable then typescript consider it a
 // Arrays 
 let numbers: number[] = []; // if array is not initialized then you must define the type of the array
 numbers[0] = 1;
+let names = ["Abhishek", "Adi"];
+names.push("Pratik");
+console.log("Arrays:");
+console.log(names);
+
 
 // Touples : are usefull when have two values like key and value pair
 let details: [number, string]  = [1, "Abhishek"];
+console.log("Touples:");
+console.log(details);
+
 
 // Enum : list of related constants
 const enum Size {Small = 1, Medium, Large};
 let mySize = Size.Large;
+console.log("Enum:");
 console.log(mySize);
+
+// Functions : have to mention type of parameter and return type of function
+// properly annotate the functions, parameters and return types
+function incomeTax(income: number): number {
+    if(income <= 50000){
+        return income * 0.1;
+    }else{
+        return income * 0.3;
+    }
+}
+
+console.log("Functions");
+const abhiTax = incomeTax(100000);
+console.log(abhiTax);
