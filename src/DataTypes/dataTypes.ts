e10c1855bd3c5f1466890a7916bf52e8578b9675
Abhishek Hulage <abhihulage14@gmail.com>
2024-06-25 10:19:48 +0530

    any data type in ts

---
+++ src/DataTypes/dataTypes.ts
@@ -0,0 +1,7 @@
+let sales = 123_456_789;
+let course = 'TypeScript';
+let is_published = true;
+
+let level;  // if we don't initialize the variable then typescript consider it as "any" type and avoid using any type
+
+// Arrays 
