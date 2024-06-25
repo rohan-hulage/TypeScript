8be6c58467e4f286a64107a50fde2af45dbf628a
Abhishek Hulage <abhihulage14@gmail.com>
2024-06-25 09:56:14 +0530

    enabled debugging

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,6 @@
 "use strict";
+let age = 22;
+if (age > 18) {
+    age += 10;
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// installed typescript using: sudo npm i -g typescript
+// created tsconfig.json i.e configuration of typescript using: tsc --init
+//  modified tsconfig.json for use of typescript features 
+
+let age: number = 22;
+
+if(age > 18) {
+    age += 10;
+    console.log(age);
+}
